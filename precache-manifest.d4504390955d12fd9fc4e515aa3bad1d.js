af911725751c8ec931bc0f4b75c59ad12f736907
Abinash Kumar <abinash@symboinsurance.com>
2020-02-15 20:38:55 +0530

    Updates

---
+++ precache-manifest.d4504390955d12fd9fc4e515aa3bad1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a812a0911d01744625230ad755c6b82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "020348b1a4a72bd39ee7",
+    "url": "/static/css/main.e6c2889f.chunk.css"
+  },
+  {
+    "revision": "0d2221d454f7356a680e",
+    "url": "/static/js/2.e31036f9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e31036f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "020348b1a4a72bd39ee7",
+    "url": "/static/js/main.e6bed87d.chunk.js"
+  },
+  {
+    "revision": "11b0edcfccaaa588779e",
+    "url": "/static/js/runtime-main.fb9310ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
